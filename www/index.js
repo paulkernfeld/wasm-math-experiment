5375c4d37ed5320fbe3dfd3fe54188407d570e40
Paul Kernfeld <paulkernfeld@gmail.com>
2020-06-07 12:36:44 -0400

    Add www as not submodule

---
+++ www/index.js
@@ -0,0 +1,10 @@
+import * as wasm from "wasm-math-experiment";
+
+let arena = new wasm.Arena();
+
+let array1 = arena.new_array_from([[1, 1, 1], [2, 2, 2]]);
+let array2 = arena.new_array_float32(new Float32Array([1, 2, 3, 1, 2, 3])); // In row-major order
+
+let array_sum = arena.add_arrays(array1, array2);
+
+arena.log_array(array_sum);
