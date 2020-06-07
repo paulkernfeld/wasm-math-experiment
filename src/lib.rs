7c0320c326d1f9df16e596010307203c84b2f6a1
Paul Kernfeld <paulkernfeld@gmail.com>
2020-06-07 10:47:48 -0400

    Theoretically add two arrays

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use ndarray::{array, Array2};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -11,6 +12,36 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub struct Arena {
+    arrays: Vec<Array2<f32>>,
+}
+
+type Handle = usize;
+
+#[wasm_bindgen]
+impl Arena {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self { arrays: Vec::new() }
+    }
+
+    pub fn new_array(&mut self) -> Handle {
+        self.arrays.push(
+            array![
+                [1.,2.,3.],
+                [4.,5.,6.],
+            ]
+        );
+        self.arrays.len() - 1
+    }
+
+    pub fn add_arrays(&mut self, array1: Handle, array2: Handle) -> Handle {
+        self.arrays.push(&self.arrays[array1] + &self.arrays[array2]);
+        self.arrays.len() - 1
+    }
 }
 
 #[wasm_bindgen]
--- tests/web.rs
+++ tests/web.rs
@@ -9,5 +9,11 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    assert_eq!(1 + 1, 2);
+    // let arena = crate::Arena::default();
+
+    // assert_eq!(a.ndim(), 2);         // get the number of dimensions of array a
+    // assert_eq!(a.len(), 6);          // get the number of elements in array a
+    // assert_eq!(a.shape(), [2, 3]);   // get the shape of array a
+    // assert_eq!(a.is_empty(), false); // check if the array has zero elements
+    // assert_eq!(1 + 1, 2);
 }
