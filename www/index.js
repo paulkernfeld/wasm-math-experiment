94cb2c93228e0952b79136d4fe49b5236bdb8c25
Paul Kernfeld <paulkernfeld@gmail.com>
2020-06-14 11:17:17 -0400

    Pure JS mapping is way faster

--- www/index.js
+++ www/index.js
@@ -13,9 +13,16 @@ let array_sum = arena.add_arrays(array1, array2);
 arena.log_array(array_sum);
 
 // Benchmark
-let long_1 = arena.new_array_float32(10000000, 1, new Float32Array(10000000));
+{
+    let long_1_wasm = arena.new_array_float32(10000000, 1, new Float32Array(10000000));
+    const t0 = performance.now();
+    let long_2_wasm = arena.map_js(long_1_wasm, x => x + 1);
+    const t1 = performance.now();
+    console.log(`Mapping with Wasm took ${t1 - t0} milliseconds.`);
+}
 
+let long_1_js = new Float32Array(10000000)
 const t0 = performance.now();
-let long_2 = arena.map_js(long_1, x => x + 1);
+let long_2_js = long_1_js.map(x => x + 1);
 const t1 = performance.now();
-console.log(`Mapping took ${t1 - t0} milliseconds.`);
+console.log(`Mapping with pure JS took ${t1 - t0} milliseconds.`);
